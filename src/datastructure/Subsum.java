6598da6e0f06f4db5d7a418670efef5b1ad44950
MARI <mari20201101@gmail.com>
2022-10-01 21:03:57 +0900

    Implement subsum of array

---
+++ src/datastructure/Subsum.java
@@ -0,0 +1,17 @@
+package datastructure;
+
+import java.util.Arrays;
+
+class Subsum {
+    public static void main(String[] args) {
+        subsum(new int[]{5,4,3,2,1});
+    }
+    static void subsum(int[] arr){
+        int[] sums = new int[arr.length];
+        sums[0] = arr[0];
+        for(int i=1; i< arr.length; i++){
+            sums[i]=arr[i]+sums[i-1];
+        }
+        System.out.println(Arrays.toString(sums));
+    }
+}
