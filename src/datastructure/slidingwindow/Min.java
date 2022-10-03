f9437fcd9cba6272ecbd25b1f7a0b3319e43c48b
MARI <mari20201101@gmail.com>
2022-10-03 12:14:40 +0900

    Practice exploit deque with pair class

--- src/datastructure/slidingwindow/Min.java
+++ src/datastructure/slidingwindow/Min.java
@@ -13,14 +13,16 @@ class Min {
         Deque<Pair> deque = new LinkedList<>();
         int idx = 0;
         deque.addLast(new Pair(idx,arr[idx]));
-        for (int i = idx; i < arr.length; i++) {
-            if(deque.peekFirst()!=null && deque.peekFirst().idx >= CAPACITY-i) deque.removeFirst();
+        for (int i = 0; i < arr.length; i++) {
+            while(deque.peekFirst()!=null && i-deque.peekFirst().idx>=CAPACITY){
+                deque.removeFirst();
+            }
             while(deque.peekLast()!= null && deque.getLast().value > arr[i]){
                 deque.removeLast();
             }
             deque.addLast(new Pair(i, arr[i]));
 
-            System.out.print(deque.getFirst().value + ",");
+            System.out.print(deque.getFirst().value+", ");
         }
     }
 }
