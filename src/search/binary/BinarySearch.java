88d33a02171e73b980901606b08e48c5ab73fd07
MARI <mari20201101@gmail.com>
2022-10-08 13:41:11 +0900

    Implement binary search

---
+++ src/search/binary/BinarySearch.java
@@ -0,0 +1,38 @@
+package search.binary;
+
+import java.util.Arrays;
+
+class BinarySearch {
+    static int[] arr = {4,1,5,2,3};
+    static int[] target = {1,3,7,9,5};
+
+    public static void main(String[] args) {
+        search();
+    }
+
+    static void search() {
+        Arrays.sort(arr);
+
+        for(int i=0; i< target.length; i++){
+            int start = 0;
+            int end = target.length-1;
+            int contain = 0;
+            while(start<=end){
+                int mid = (start + end)/2;
+                if(target[i] == arr[mid]) {
+                    contain = 1;
+                    break;
+                }
+                if(target[i] < arr[mid]){
+                    end = mid-1;
+                    continue;
+                }
+                if(target[i] > arr[mid]){
+                    start = mid+1;
+                    continue;
+                }
+            }
+            System.out.println(contain==1? 1 : 0);
+        }
+    }
+}
