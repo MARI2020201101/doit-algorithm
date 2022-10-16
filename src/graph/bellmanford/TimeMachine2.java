2cb71e7aac5c968826b942177baaa552f3ff3a36
MARI <mari20201101@gmail.com>
2022-10-16 16:37:13 +0900

    Design of detecting negative cycle of Bellman-Ford Algorithm

---
+++ src/graph/bellmanford/TimeMachine2.java
@@ -0,0 +1,35 @@
+package graph.bellmanford;
+
+import java.util.Arrays;
+
+class TimeMachine2 {
+    static int N = 4;
+    static int[][] arr ={
+            {0,0,0,0},
+            {0,0,4,3},
+            {0,0,0,-4},
+            {0,-2,0,0}
+    };
+
+    static int[] answer = new int[N];
+    static int[] visited = new int[N];
+    static {
+        Arrays.setAll(answer, a->Integer.MAX_VALUE);
+    }
+
+    public static void main(String[] args) {
+        bellmanFord(1, 0);
+        System.out.println(Arrays.toString(answer));
+    }
+    static void bellmanFord(int i, int val){
+        for (int j = 1; j < N; j++) {
+            if(arr[i][j]!=0 && visited[j] < 3) {
+                visited[j]++;
+                answer[j] = Math.min(answer[j],val+arr[i][j]);
+                bellmanFord(j, val+arr[i][j]);
+
+            }
+        }
+
+    }
+}
