a395db2c134a05ca7081acabac91dffc0c1d8db6
MARI <mari20201101@gmail.com>
2022-10-19 15:17:48 +0900

    Solve painting island for count

--- src/graph/minimumspanningtree/Island.java
+++ src/graph/minimumspanningtree/Island.java
@@ -12,7 +12,7 @@ class Island {
             {0,0,0,0,0,0,0,0},
             {1,1,1,1,1,1,1,1}
     };
-    static int[][] islandMap = new int[N][M];
+    static boolean[][] visited = new boolean[N][M];
     static int islandCount = 0;
     static int[] X = {1,0,-1,0};
     static int[] Y = {0,-1,0,1};
@@ -21,14 +21,16 @@ class Island {
         island();
         print();
     }
-    //1. 각각의 섬을 구분한다.
+
     static void island(){
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if(map[i][j]!=0){
-                    islandMap[i][j] = ++islandCount;
+                if(map[i][j]!=0 && !visited[i][j]){
+                    visited[i][j] = true;
+                    map[i][j] = ++islandCount;
                     move(i,j);
                 }
+                visited[i][j] = true;
             }
         }
     }
@@ -37,15 +39,17 @@ class Island {
         for (int k = 0; k < X.length; k++) {
             int nextX = i + X[k];
             int nextY = j + Y[k];
-            if(nextX >=0 && nextX < N && nextY >= 0 && nextY < M && map[nextX][nextY]==1){
-                islandMap[nextX][nextY] = islandCount;
+            if(nextX >=0 && nextX < N && nextY >= 0 && nextY < M && map[nextX][nextY]!=0 && !visited[nextX][nextY]){
+                visited[nextX][nextY] = true;
+                map[nextX][nextY] = islandCount;
+                move(nextX, nextY);
             }
         }
     }
     private static void print() {
-        for (int i = 0; i < islandMap.length; i++) {
-            for (int j = 0; j < islandMap[0].length; j++) {
-                System.out.print(islandMap[i][j] +" ");
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                System.out.print(map[i][j] +" ");
             }
             System.out.println();
         }
