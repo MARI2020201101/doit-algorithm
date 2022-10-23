b8b3278fb6a888fa42856927d0c5d53e69f5bb05
MARI <mari20201101@gmail.com>
2022-10-23 10:40:45 +0900

    Refactor segment tree

--- src/tree/segment/update/SegmentTree.java
+++ src/tree/segment/update/SegmentTree.java
@@ -6,7 +6,7 @@ class SegmentTree {
     static int[] arr = {1,2,3,4,5};
     static int N = arr.length;
 
-    static int[] subsum = new int[(int) (Math.pow(2,(Math.log10(N)/Math.log10(2))+1))];
+    static int[] subsum = new int[(int) Math.pow(2,(Math.floor(Math.log(N)/Math.log(2)+1))+1)];
     static int M = subsum.length;
 
     public static void main(String[] args) {
@@ -18,11 +18,10 @@ class SegmentTree {
     static void update(int idx, int val){
         arr[idx] = val;
         int sIdx = M / 2 + idx;
-        System.out.println("sIdx = "+sIdx);
         int original = subsum[sIdx];
         int diff = val-original;
         subsum[sIdx] = val;
-        while(sIdx/2 >0){
+        while(sIdx/2 > 0){
             sIdx=sIdx/2;
             subsum[sIdx] += diff;
         }
@@ -30,7 +29,7 @@ class SegmentTree {
 
 
     static void segmentTree(){
-        for (int i = N-1,j = M-1; i >= 0 ; i--,j--) {
+        for (int i = 0,j = M/2; i < N ; i++,j++) {
             subsum[j] = arr[i];
         }
         for (int i = M/2-1; i >= 0 ; i--) {
