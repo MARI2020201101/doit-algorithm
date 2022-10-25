513dd9659b31a2c09a6b8a5e4fbeefe4620cce47
MARI <mari20201101@gmail.com>
2022-10-25 18:04:29 +0900

    Practice segment tree with binary search

--- src/tree/segment/submul/SegmentTree.java
+++ src/tree/segment/submul/SegmentTree.java
@@ -1,22 +1,32 @@
 package tree.segment.submul;
 
+import java.util.Arrays;
+
 class SegmentTree {
     static int[] arr = {1,2,3,4,5};
     static int[] tree = new int[lenOfTree(arr)];
 
     public static void main(String[] args) {
         System.out.println(lenOfTree(arr));
+        createMultiplySegmentTree(tree);
+        System.out.println(Arrays.toString(tree));
     }
     static int lenOfTree(int[] arr){
         int arrLen = arr.length;
         return (int) (Math.pow(2, Math.ceil(Math.log(arrLen)/Math.log(2))+1));
     }
     static void createMultiplySegmentTree(int[] tree){
-
+        Arrays.setAll(tree, a -> 1);
+        createMultiplySegmentTree(tree, 1, 0, arr.length-1);
     }
-    static private void createMultiplySegmentTree(int[] tree, int node, int start, int end){
+    private static int createMultiplySegmentTree(int[] tree, int node, int start, int end){
         if(start == end){
-
+            return tree[node] = arr[start];
+        }
+        else {
+            return tree[node] =
+                    createMultiplySegmentTree(tree, node*2 , start, (start+end)/2)
+                     * createMultiplySegmentTree(tree, node*2+1 , (start+end)/2+1, end);
         }
     }
 }
