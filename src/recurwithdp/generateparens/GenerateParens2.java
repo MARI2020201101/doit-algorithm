8add6b643004c8e3ecba2b943df3b098aa95ac1b
MARI <mari20201101@gmail.com>
2022-11-05 12:10:47 +0900

    Study generate parens with recursion

---
+++ src/recurwithdp/generateparens/GenerateParens2.java
@@ -0,0 +1,26 @@
+package recurwithdp.generateparens;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class GenerateParens2 {
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<>();
+        int size = 5;
+        char[] parens = new char[size*2];
+        generateParens(0,size,size,parens,set);
+        set.forEach(System.out::println);
+    }
+    static void generateParens(int index, int left, int right, char[] parens, Set<String> set){
+        if(left < 0 || left > right) return;
+        if(left == 0 && right == 0) {
+            set.add(String.copyValueOf(parens));
+            return;
+        }
+        parens[index] = '(';
+        generateParens(index+1, left-1, right, parens, set);
+        parens[index] = ')';
+        generateParens(index+1, left, right-1, parens, set);
+
+    }
+}
